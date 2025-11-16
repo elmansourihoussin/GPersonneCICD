29e4029dc56768f7b4b1429d41a3b17c317eb95c
elmansourihoussin <houssin.el.mansouri@gmail.com>
2025-11-16 21:23:04 +0100

    seconde commit

---
+++ src/main/java/com/elmansouri/gpersonnecicd/GPersonneCicdApplication.java
@@ -0,0 +1,13 @@
+package com.elmansouri.gpersonnecicd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GPersonneCicdApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GPersonneCicdApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/elmansouri/gpersonnecicd/GPersonneCicdApplicationTests.java
@@ -0,0 +1,13 @@
+package com.elmansouri.gpersonnecicd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GPersonneCicdApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
